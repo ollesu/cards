f3ffb25bff6cd32f02cd379a2cc510af96ae8722
Aisa Mukukenova <mkaysa@gmail.com>
2019-06-28 15:02:01 +0100

    added iteration to create cards

--- deck.go
+++ deck.go
@@ -6,8 +6,27 @@ import "fmt"
 // (which is / has a behaviour of a slice of strings)
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+// this is a custom method (receiver) for type 'deck'
+// any variable of the type deck in our app now gets access to print function
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
+
+// d is a working instance of / reference to the deck variable
+// in this example, slice cards is a d variable

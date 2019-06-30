1af9b784bf897edf2014f3f8c5b15d0eaada0b98
Aisa Mukukenova <mkaysa@gmail.com>
2019-06-30 21:49:16 +0100

    finished writing 2 tests for checking the generation of cards

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,13 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 28 {
 		t.Errorf("expected deck length of 16 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("expected first card to be Ace of Spades but got %v", d[0])
+	}
+
+	// len(d) - 1 refers to the last element of the slice
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("expected last card to be King of Clubs but got %v", d[len(d)-1])
+	}
 }

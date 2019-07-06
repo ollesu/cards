fbee9cf273049128534ca2ee5e3ab43e62fcf6a1
Aisa Mukukenova <mkaysa@gmail.com>
2019-07-06 22:00:50 +0100

    wrote test and remove file

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -17,4 +20,16 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("expected last card to be King of Clubs but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 28 {
+		t.Errorf("Expected 28 cards in the deck, got %d", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
+
 }
